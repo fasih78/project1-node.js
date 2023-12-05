252c12fa78c3015194324144e9ff76c7ce43ee13
fasih78 <ssmfaseh@gmail.com>
2023-12-05 17:29:07 +0500

    updates

--- DashBoard/user.js/userservice.js
+++ DashBoard/user.js/userservice.js
@@ -5,7 +5,9 @@ const saltRounds = 10;
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const twilio = require("twilio");
-const crypto = require('crypto');
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 // const SignUp = async (req, res) => {
 //   try {
@@ -100,19 +102,14 @@ const SignUp = async (req, res) => {
     const LastUser = await UserModel.findOne().sort({ _id: -1 }).exec();
     const id = LastUser ? LastUser.id + 1 : 1;
 
-
     function generateOTP() {
       const otpBuffer = crypto.randomBytes(3); // Adjust the number of bytes as needed
       const otp = otpBuffer.readUIntBE(0, otpBuffer.length) % 1000000;
-    
-      return otp.toString().padStart(6, '0');
+
+      return otp.toString().padStart(6, "0");
     }
-    
+
     const otpGenerated = generateOTP();
-
-
-
-
 
     // Generate OTP and send via Twilio
     // const otpGenerated = Math.floor(100000 + Math.random() * 900000);
@@ -121,15 +118,19 @@ const SignUp = async (req, res) => {
     const client = require("twilio")(accountSid, authToken);
 
     try {
-      const message = await client.verify.v2
-        .services(process.env.TWILIO_SERVICE_SID)
-        .verifications.create({
-          to: phonenumber,
-          channel: "sms", 
-          body: `hELLOW`
-        });
+      // const message = await client.verify.v2
+      //   .services(process.env.TWILIO_SERVICE_SID)
+      //   .verifications.create({
+      //       to: phonenumber,
+      //       channel: "sms",
+      //       body: `hELLOW`
+      //     });
 
-      console.log("Twilio API Response:", message);
+      const message = await client.messages.create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phonenumber,
+        body: "hellow",
+      });
 
       const token = jwt.sign({ email: email }, process.env.JWT_SECRET_KEY, {
         expiresIn: "365d",
@@ -151,12 +152,11 @@ const SignUp = async (req, res) => {
         TwilioAPIResponse: message,
       });
     } catch (twilioError) {
-      console.log("Error sending OTP:", twilioError);
-      return res.status(500).json({ error: "Failed to send OTP." });
+      return res.status(401).send({ message: twilioError });
     }
   } catch (err) {
     console.error("Error in SignUp:", err);
-    res.status(500).json({ error: "An internal server error occurred." });
+    res.status(500).json({ error: "An error occurred." });
   }
 };
 
@@ -187,15 +187,36 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const findemail = await UserModel.findOne({ email: email }).exec();
-    console.log(findemail);
+
     if (findemail) {
       const hash = findemail.password;
       const passwordcompared = await bcrypt.compare(password, hash);
-      console.log(passwordcompared);
       if (passwordcompared) {
         const findpassword = await UserModel.findOne({
           password: passwordcompared,
         }).exec();
+
+        var transporter = nodemailer.createTransport({
+          service: "outlook",
+          auth: {
+            user: process.env.ADMIN_EMAIL,
+            pass: process.env.ADMIN_EMAIL_PASSWORD,
+          },
+        });
+        const info1 = await transporter.sendMail({
+          from: "career@fascom.com", // sender address
+          to: "career@fascom.com", // list of receivers
+          subject: `Hello !  ${email}`, // Subject line
+          text: `Welcome to Fascom Limited
+          Thank you for interest in empolyment at Fascom Limited. if your qualifiaction
+          match our needs , we will contact you to learn more about your fit in this position
+          
+          Thanks again for your inquiry!
+          
+          - Fascom Limited
+          ,
+          `,
+        });
         res.status(200).send({ message: "login!" });
       } else {
         res.status(400).send({ message: "Wrong Password!" });
